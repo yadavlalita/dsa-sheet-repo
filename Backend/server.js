643ca125c2fcc7378f2508a0900df505bcc3e669
Lalita Yadav <yadavlalita803@gmail.com>
2025-04-28 12:03:46 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-
+const PORT = process.env.PORT || 5000;
 const authRoutes = require('./routes/auth');
 const topicRoutes = require('./routes/topic');
 
@@ -22,6 +22,6 @@ mongoose.connect(process.env.MONGO_URI,{
 })
 .then(() => {
     console.log('MongoDB Connected');
-    app.listen(5000, () => console.log('Server running on port 5000'));
+    app.listen(PORT, () => console.log('Server running on port 5000'));
 })
 .catch((err) => console.log(err));
